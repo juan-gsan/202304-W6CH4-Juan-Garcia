487895f5c911f59befec351e159c02d649b4fd91
JuanG <juan.gsanchez13@gmail.com>
2023-06-08 23:39:51 +0200

    Add calculator to server

---
+++ src/calculator.ts
@@ -0,0 +1,6 @@
+export const calculator = (a: number, b: number) => ({
+  sum: a + b,
+  difference: a - b,
+  multiply: a * b,
+  division: a / b,
+});
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,43 @@
 import http from 'http';
 import * as dotenv from 'dotenv';
 import url from 'url';
+import { calculator } from './calculator.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 9999;
 
 const server = http.createServer((req, res) => {
-  const { path } = url.parse(req.url as string);
+  if (!req.url) {
+    server.emit('error', new Error('No url in the request'));
+    return;
+  }
 
-  if (path !== '/calculator') {
+  const { pathname, query } = url.parse(req.url as string, true);
+
+  if (pathname !== '/calculator') {
     server.emit('error', new Error('404'));
-    res.end();
-    return;
+    res.statusCode = 404;
+    res.end('Error. Not found.');
+    process.exit(-1);
   }
 
-  if (path === '/calculator') {
-    res.write(`<h1>Calculator</h1>`);
-    return;
+  const a = parseInt(query.a as string, 10);
+  const b = parseInt(query.b as string, 10);
+
+  if (isNaN(a) || isNaN(b)) {
+    res.statusCode = 400;
+    res.end('Error. Invalid input');
+    process.exit(-1);
   }
 
-  if (path === '/calculator?a=6&b=3') {
-    res.write(`<h1>Calculator</h1>`);
-    res.write(`<h2>Resultados:</h2>`);
-    return;
-  }
+  const calculatorResult = calculator(a, b);
 
+  res.write(`<h1>Calculator</h1>`);
+  res.write(`<p>${a} + ${b} = ${calculatorResult.sum}</p>`);
+  res.write(`<p>${a} - ${b} = ${calculatorResult.difference}</p>`);
+  res.write(`<p>${a} x ${b} = ${calculatorResult.multiply}</p>`);
+  res.write(`<p>${a} / ${b} = ${calculatorResult.division}</p>`);
   res.end();
 });
 
